18027ba49708bede73c0257ffc15bb5c6ce87623
rahuldamodar94 <rahuldamodar94@gmail.com>
2019-07-25 16:47:19 +0530

    tron get bandwidth and energy balance

--- test.js
+++ test.js
@@ -39,29 +39,21 @@ const tronWeb = new TronWeb({
 //     console.log(err.message)
 // })
 
-// axios({
-//     method: 'post',
-//     url: 'https://api.shasta.trongrid.io/wallet/getaccountnet',
-//     data: {
-//         address: tronWeb.address.toHex("TQMfW11u15HnkbsYYrMyyMDsqidGjZTSKV"),
-//     },
-// }).then(function(response) {
-//     console.log(response.data);
+tronWeb.trx.getAccountResources("TQMfW11u15HnkbsYYrMyyMDsqidGjZTSKV").then(res => {
+	console.log(res);
+})
+
+
+// tronWeb.trx.sendTransaction(tronWeb.address.toHex("TAHfJybMZuDZ1Cv7pyzad9G4AXufjMKfb2"),
+//     parseInt(tronWeb.toSun(100)), "F69C12C65B027FCE8AFBC3AC67F8E111E7E6C49DA392DF4FAAEA8E2B32E0D9D1").then(res => {
+//     console.log(res);
 // }).catch(err => {
 //     console.log(err.message)
 // })
 
+// const app = async () => {
+//     const userBalance = await tronWeb.trx.getBalance(tronWeb.address.toHex("TQMfW11u15HnkbsYYrMyyMDsqidGjZTSKV"));
+//     console.log(`User's balance is: ${ userBalance/1000000 }`);
+// };
 
-tronWeb.trx.sendTransaction(tronWeb.address.toHex("TAHfJybMZuDZ1Cv7pyzad9G4AXufjMKfb2"),
-    parseInt(tronWeb.toSun(100)), "F69C12C65B027FCE8AFBC3AC67F8E111E7E6C49DA392DF4FAAEA8E2B32E0D9D1").then(res => {
-    console.log(res);
-}).catch(err => {
-    console.log(err.message)
-})
-
-const app = async () => {
-    const userBalance = await tronWeb.trx.getBalance(tronWeb.address.toHex("TQMfW11u15HnkbsYYrMyyMDsqidGjZTSKV"));
-    console.log(`User's balance is: ${ userBalance/1000000 }`);
-};
-
-app();
+// app();
