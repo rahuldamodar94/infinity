ce7ccd8c415d464b2fa6d6e0c681ae5a32cbfa5e
rahuldamodar94 <rahuldamodar94@gmail.com>
2019-07-25 21:32:22 +0530

    btc

---
+++ btc.js
@@ -0,0 +1,16 @@
+var axios = require('axios');
+
+
+var balance = function(address) {
+
+    axios({
+        method: 'get',
+        url: 'https://api.blockcypher.com/v1/btc/main/addrs/' + address + '/balance',
+    }).then(function(response) {
+        console.log(response.data.balance / 100000000);
+    }).catch(err => {
+        console.log(err.message)
+    })
+}
+
+balance('1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD');
