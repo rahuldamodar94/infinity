1b9db433ef739a41e41896769f8d9920005b321d
rahuldamodar94 <rahuldamodar94@gmail.com>
2019-07-26 16:35:24 +0530

    eos balance

--- eos.js
+++ eos.js
@@ -10,10 +10,20 @@
 // console.log("publicKey: "+ecc.PublicKey(node._publicKey).toString())
 // console.log("privateKey: "+wif.encode(128, node._privateKey, false))
 
-const fetch = require('node-fetch');
-const { JsonRpc } = require('eosjs');
+// const fetch = require('node-fetch');
+// const { JsonRpc } = require('eosjs');
 
-// Instantiate a new JsonRpc object, with the Network Api Uri, and a request object
-const rpc = new JsonRpc('https://api.eosnewyork.io', { fetch });
-// Request the balance, passing in the token contract, the account name, and the token symbol
-rpc.get_currency_balance('eosio.token', 'eospaceioeos', 'EOS').then((balance) => console.log(balance));
+//eospaceioeos
+
+var balance = fucntion(account_name) {
+    // Instantiate a new JsonRpc object, with the Network Api Uri, and a request object
+    const rpc = new JsonRpc('https://api.eosnewyork.io', { fetch });
+    // Request the balance, passing in the token contract, the account name, and the token symbol
+    rpc.get_currency_balance('eosio.token', account_name, 'EOS').then((balance) => console.log(balance));
+}
+
+
+
+// EosApi = require('eosjs-api') // Or EosApi = require('./src')
+
+// eos = EosApi()
